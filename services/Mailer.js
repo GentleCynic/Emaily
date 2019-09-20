136050ffcb8257f1079b981e58f52332acf5ea86
GentleCynic <bencraft101@gmail.com>
2019-09-20 14:13:45 +0100

    final with blank heroku page

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail{
       super();
 
       this.sgApi = sendgrid(keys.sendGridKey);
-      this.from_email = new helper.Email('no-reply@gchq.co.uk');
+      this.from_email = new helper.Email('no-reply@emaily.co.uk');
       this.subject = subject;
       this.body = new helper.Content('text/html', content);
       this.recipients = this.formatAddresses(recipients);
